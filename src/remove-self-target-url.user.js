42eff1c384e0755c3bf158e65fdd19f63cad115a
cologler <Cologler@users.noreply.github.com>
2017-04-05 15:34:45 +0800

    Update remove-self-target-url.user.js

--- src/remove-self-target-url.user.js
+++ src/remove-self-target-url.user.js
@@ -24,30 +24,4 @@
     };
 
     mainLogic();
-})();// ==UserScript==
-// @name                   html-fixed: remove-self-target-url
-// @namespace              https://github.com/cologler/html-fixed
-// @version                1.0
-// @description            remove href which target to self
-// @description:zh-CN      移除指向自身的超链接
-// @author                 cologler
-// @include                *
-// @grant                  none
-// ==/UserScript==
-
-(function() {
-    'use strict';
-
-    var mainLogic = function() {
-        let cur = location.href;
-        let as = document.querySelectorAll('a');
-        for (var i = 0; i < as.length; i++) {
-            let a = as[i];
-            if (a.href == cur) {
-                a.removeAttribute('href');
-            }
-        }
-    };
-
-    mainLogic();
 })();
